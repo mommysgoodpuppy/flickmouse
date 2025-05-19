0b407347ba5a39b65b7ad33fb9c7fbd5a18dd703
mommysgoodpuppy <theelectronicfreezer@gmail.com>
2025-05-19 22:38:33 +0300

    yeet

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import deno from '@deno/vite-plugin'
 import react from '@vitejs/plugin-react'
-import process from "node:process";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/" + process.env.GITHUB_REPOSITORY?.split("/").pop() || "{app_name}",
   plugins: [deno(), react()],
 })
