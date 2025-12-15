29e341046e503c8353f0669e9435b0f01da60635
mommysgoodpuppy <theelectronicfreezer@gmail.com>
2025-12-15 21:27:45 +0200

    esnext

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
-import deno from '@deno/vite-plugin'
+//import deno from '@deno/vite-plugin'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [deno(), react()],
+  plugins: [react()], //deno()
+  build: {
+    target: 'esnext',
+  },
 })
